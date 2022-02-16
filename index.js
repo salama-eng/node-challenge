2a4d0b18fc3fcc1f4dfd28fd5294714962ca48f7
salama <a541996salamh@gmail.com>
2022-02-16 16:07:41 +0300

    commit

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
+const express=require("express");
 
-const http = require('http');
-const fs = require('fs');
-
-
-const file = (fileName) => fs.createReadStream(fileName, 'utf-8');
-
-http
-  .createServer((req, res) => {
-    if (req.url.startsWith('/style.css')) {
-      res.setHeader('Content-Type', 'text/css');
-      file('./assets/css/style.css').pipe(res);
-    }
-    else if(req.url.startsWith('/vendor.css')) {
-        res.setHeader('Content-Type', 'text/css');
-      file('./assets/css/vendor.css').pipe(res);
-    }
-
-    if (req.url.startsWith('/admin')) {
-      if (req.url === '/admin') {
-        file('./assets/admin.html').pipe(res);
-      } else {
-        file('./assets/login.html').pipe(res);
-      }
-    } else if (req.url === '/index.html') {
-      file('./assets/index.html').pipe(res);
-    
-    } else if (req.url === '/contact.html') {
-        file('./assets/contact.html').pipe(res); }
-    else if (req.url === '/about.html') {
-      file('.assets/admin.html').pipe(res);
-    } 
-    else {
-      file('./assets/readme.txt').pipe(res);
-    }
-  })
-  .listen(3000);
-
-
+const app=express();
+app.set('view engine','ejs');
+app.set('views','view');
+app.listen("3000");
+app.use('/',express.static('view/css'));
+app.use('/assets',express.static('view/assets'));
+app.get("/",(req,res)=>{
+res.render("index");
+});
