742140b08c6db8890b1abee3bd410dd19486f808
salama <a541996salamh@gmail.com>
2022-02-17 16:14:48 +0300

    commit

--- index.js
+++ index.js
@@ -5,35 +5,39 @@ const fs = require('fs');
 
 const file = (fileName) => fs.createReadStream(fileName, 'utf-8');
 
-http
-  .createServer((req, res) => {
-    if (req.url.startsWith('/style.css')) {
-      res.setHeader('Content-Type', 'text/css');
-      file('./assets/css/style.css').pipe(res);
-    }
-    else if(req.url.startsWith('/vendor.css')) {
-        res.setHeader('Content-Type', 'text/css');
-      file('./assets/css/vendor.css').pipe(res);
-    }
+const express=require("express");
 
+const app=express();
+app.set('view engine','ejs');
+app.set('views','view');
+app.listen("3001");
+app.use('/',express.static('view/css'));
+app.use('/images',express.static('view/images'));
+app.use('/',express.static('view/js'));
+app.use('/fontawesome',express.static('view/js/fontawesome'));
+app.get("/",(req,res)=>{
+res.render("index");
+res.end();
+});
+
+
+app.get('/about', (req, res)=> {
+  res.render('about');
+});
+
+app.get('/contact', (req, res)=> {
+  res.render('contact');
+});
+
+http.createServer((req, res) => {
     if (req.url.startsWith('/admin')) {
       if (req.url === '/admin') {
-        file('./assets/admin.html').pipe(res);
-      } else {
-        file('./assets/login.html').pipe(res);
+        file('admin.html').pipe(res);
+      } else{
+        file('login.html').pipe(res);
       }
-    } else if (req.url === '/index.html') {
-      file('./assets/index.html').pipe(res);
     
-    } else if (req.url === '/contact.html') {
-        file('./assets/contact.html').pipe(res); }
-    else if (req.url === '/about.html') {
-      file('.assets/admin.html').pipe(res);
-    } 
-    else {
-      file('./assets/readme.txt').pipe(res);
+   
     }
-  })
-  .listen(3000);
 
-
+  });
